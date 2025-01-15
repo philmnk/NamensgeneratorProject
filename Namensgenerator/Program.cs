7f6dbb7e98719bb9ef434bc55201ca4fb682a8aa
philmnk <manke.phil@t-online.de>
2025-01-15 14:22:42 +0100

    Anlegen von Project

---
+++ Namensgenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static string[] firstnamesFemale = { "Aeloria", "Lyrielle", "Sylvara", "Elunara", "Seraphina", "Nymira", "Thalindra", "Isoldea", "Veylana", "Celyndra" };
+    static string[] firstnamesMale = { "Kaelion", "Darakor", "Fenrik", "Thalion", "Ravendryn", "Orinvar", "Brynthar", "Zarathos", "Eryndor" };
+    static string[] lastnames = { "Dämmerhain", "Nachtklinge", "Silbersturm", "Morgentau", "Eisenschlag", "Dunkelquell", "Flammenfels", "Sturmschatten" };
+
+    static void Main(string[] args)
+    {
+        
+    }
+}
